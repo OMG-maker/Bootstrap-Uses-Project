6d002c6e65b9512648c1ad2775a4e1a9588e28d0
mgOh <83509684+OMG-maker@users.noreply.github.com>
2022-01-06 20:52:42 +0900

    add config, model, repo, test, controller

--- src/main/java/com/example/vaccinesideeffectswebsite/VaccineSideEffectsWebsiteApplication.java
+++ src/main/java/com/example/vaccinesideeffectswebsite/VaccineSideEffectsWebsiteApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VaccineSideEffectsWebsiteApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(VaccineSideEffectsWebsiteApplication.class, args);
     }
-
 }
