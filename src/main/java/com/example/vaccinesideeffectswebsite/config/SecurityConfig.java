35adff200db3fdeac3d2d4581916833403e2df1b
mgOh <83509684+OMG-maker@users.noreply.github.com>
2022-01-11 15:55:12 +0900

    IndexController,ViewController edit
    ViewController, RestController 에 옮겼던 메소드들이 IndexController에 다시 옮겨가졌을때 정상작동하는것을 확인. 다시 옮기면서 체크할것.

--- src/main/java/com/example/vaccinesideeffectswebsite/config/SecurityConfig.java
+++ src/main/java/com/example/vaccinesideeffectswebsite/config/SecurityConfig.java
@@ -14,15 +14,15 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableWebSecurity // 필터 체인 관리 시작 어노테이션
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true) // 특정 주소 접근시 권한 및 인증을 위한 어노테이션 활성화
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
-	
+
 	@Autowired
 	private PrincipalOauth2UserService principalOauth2UserService;
-	
+
 	@Bean
 	public BCryptPasswordEncoder encodePwd() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		//httpSecurity.httpBasic().disable();  // 기존 SecurityConfig 에서 사용하던 강제 login 무효화
@@ -38,8 +38,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.loginPage("/login")
 //				.usernameParameter("username2") // 해당 코드를 사용하면 PrincipalDetailsService 에서 username 이 아닌 username2 를 사용해야한다.
 			.loginProcessingUrl("/loginProc") // loginProc 주소가 호출이 되면 시큐리틱 낚아채서 대신 로그인을 진행함
-			.defaultSuccessUrl("/") // 실험용
-//			.defaultSuccessUrl("/index") // 성공했을때 이동하는 페이지
+			.defaultSuccessUrl("/") // 성공했을때 이동하는 페이지
 
 		.and()
 			.oauth2Login()
