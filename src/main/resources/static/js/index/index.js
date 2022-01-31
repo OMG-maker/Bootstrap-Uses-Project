91baceb01e15223d498e18a15814bf0a52870679
mgOh <83509684+OMG-maker@users.noreply.github.com>
2022-01-31 23:58:11 +0900

    index 페이지 제목 변경

--- src/main/resources/static/js/index/index.js
+++ src/main/resources/static/js/index/index.js
@@ -8,8 +8,8 @@ console.log(dash.text());
 let session = sessionStorage.getItem("JSESSIONID");
 console.log(session);
 
-// function checkMySession() {
-//     console.log(dash.text());
-//     dash.text('ㅁㄴㅇ');
-// }
-// checkMySession();
+function checkMySession() {
+    console.log(dash.text());
+    dash.text('백신 접종 후 건강상태 현황');
+}
+checkMySession();
